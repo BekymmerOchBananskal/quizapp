6ce1290cc6c5b8546a542afebaa89143bc84643b
atili <atili@ATILIM.local>
2025-11-29 21:30:01 +0300

    add Gradable interface

--- src/quizapp/Gradable.java
+++ src/quizapp/Gradable.java
@@ -1,5 +1,16 @@
 package quizapp;
 
+/**
+ * Bir quzin puan hesaplama özelliğini temseil eden arayüz.
+* Bu arayüzü uygulayan sınıflar puan hesaplıyabilir.
+*/
+ 
 public interface Gradable {
-
+	
+	/**
+	 * Öğrenciniin aldığı toplam puanı hesaplar.
+	 * @return int toplam puan
+	 * 
+	 */
+	int calculateScore();
 }

221f29caf0b76e9b0a57ddd954e0b64f64f6d4a7
atili <atili@ATILIM.local>
2025-12-05 13:23:59 +0300

    Update Student class

--- src/quizapp/Student.java
+++ src/quizapp/Student.java
@@ -1,5 +1,42 @@
 package quizapp;
 
+/**
+ * ğrenciyi temsil eden sınıf.
+ * Öğrencinin adı ve aldığı toplam puan bilgisini taşır.
+ */
 public class Student {
-
+	private String name;
+	private int totalScore;
+	
+	/**
+	 * Öğrenciyi oluşturur.
+	 * @param name öğrenci adı.
+	 */
+	public Student(String name) {
+		this.name=name;
+	}
+	
+	/**
+	 * Öğrencinin adını döndürür.
+	 * @return öğrenci adı
+	 */
+	public String getname() {
+		return name;
+	}
+	
+	/**
+	 * Öğrencinin puanını günceller.
+	 * @param score yeni puan.
+	 */
+	public void setScore(int score) {
+		this.totalScore=score;
+	}
+	
+	/**
+	 * Öğrencinin toplam puanını döndürür.
+	 * @return toplam puan
+	 */
+	public int getScore() {
+		return totalScore;
+	}
 }
